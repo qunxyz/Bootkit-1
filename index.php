87d26246b19d940d076a5c89070b496b9d8da620
Eric Range <eric.range@live.de>
2014-05-17 19:01:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
 
 require("cms/router.php");
+
+$Title = "PHP Bootstrap";
+$Description = "Starter kit for PHP";
+
 $Page = isset($_GET["page"]) ? $_GET["page"] : "";
 $router = new Router($Page);
 $router->init();
