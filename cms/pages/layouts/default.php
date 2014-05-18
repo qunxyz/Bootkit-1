fa2bdb7521a611f7218172850ad10bd6df90c057
Eric Range <mail@ericrange.de>
2014-05-18 21:16:21 +0200

    Update

--- cms/pages/layouts/default.php
+++ cms/pages/layouts/default.php
@@ -1,7 +1,7 @@
 <?php global $Title, $MetaDescription; ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="de">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
